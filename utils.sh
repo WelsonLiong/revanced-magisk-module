f4ca4f2a4d18f9d56ff1ea52b33ce792837e6486
Welson Liong <89066255+WelsonLiong@users.noreply.github.com>
2025-12-29 20:07:28 +0800

    Revert changes

--- utils.sh
+++ utils.sh
@@ -610,17 +610,6 @@ build_rv() {
 		fi
 		if [ "$build_mode" = apk ]; then
 			local apk_output="${BUILD_DIR}/${app_name_l}-${rv_brand_f}-v${version_f}-${arch_f}.apk"
-			
-			if [ "$app_name" = "YouTube" ]; then
-				apk_output="${BUILD_DIR}/youtube.apk"
-			elif [ "$app_name" = "Music" ]; then
-				apk_output="${BUILD_DIR}/music.apk"
-			elif [ "$app_name" = "TikTok" ]; then
-				apk_output="${BUILD_DIR}/tiktok.apk"
-			else
-				apk_output="${BUILD_DIR}/${app_name_l}-${rv_brand_f}-v${version_f}-${arch_f}.apk"
-			fi
-
 			mv -f "$patched_apk" "$apk_output"
 			pr "Built ${table} (non-root): '${apk_output}'"
 			continue
